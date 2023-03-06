206ffc6bf27c2c6e774633fbe0ad67d89f2a6adc
Maninder Kaur <manikathuria2001@gmail.com>
2023-03-06 17:06:57 +0530

    removing comments

--- modules/task/joi_models.js
+++ modules/task/joi_models.js
@@ -1,23 +1,16 @@
 const joi = require('joi');
 
-//authorization bearer token is required in body as passing it in header is not allowed as per the guideline 
+
 module.exports = {
     createTaskReq: joi.object({
         task: joi.string().required(),
         date: joi.date().required(),
         status: joi.boolean(),
-        // token: joi.string().required()
     }).required(),
 
     readTaskReq: joi.object({
-        // filter: joi.object({
-        //     task: joi.string(),
-        //     date: joi.date(),
-        //     status: joi.boolean(),
-        // }),
         perPage: joi.number().required(),
         page: joi.number().required(),
-        // token: joi.string().required()
     }).required(),
 
     updateTaskReq: joi.object({
@@ -25,7 +18,6 @@ module.exports = {
         task: joi.string(),
         date: joi.date(),
         status: joi.boolean(),
-        // token: joi.string().required()
     }).required(),
 
     deleteTaskReq: joi.object({
@@ -33,7 +25,6 @@ module.exports = {
         task: joi.string(),
         date: joi.date(),
         status: joi.boolean(),
-        // token: joi.string().required()
     }).required(),
 
     sortTaskReq: joi.object({
