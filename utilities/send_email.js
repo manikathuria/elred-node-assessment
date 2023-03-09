dcbd0fb44456b6f0af793e369356d153ea9efdd7
Maninder Kaur <manikathuria2001@gmail.com>
2023-03-09 08:46:24 +0530

    email service changed

--- utilities/send_email.js
+++ utilities/send_email.js
@@ -1,35 +1,44 @@
-const mailer = require('@sendgrid/mail');
-const path = require("path")
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
+var path = require("path");
+require('dotenv').config({ path: __dirname + '/.env' });
 const logger = require('./logger/logger');
+const nodemailer = require('nodemailer');
 
-mailer.setApiKey(process.env.SENDGRID_API_KEY);
+module.exports = async (user, type, token = null) => {
+    let html = `<strong>error in send email</strong>`
+    let subject = "error in send email";
 
-module.exports = async (userObject, type, token = null) => {
-  let html = `<strong>error in send email</strong>`
-  let subject = "error in send email";
+    if (type === "user_otp_verification") {
+        subject = `Verify your email`
+        html = `Your OTP is <b>${token} </b>. It will expire in 1 hour.`
+    }
+    if (type === "user_forgot_password") {
+        subject = 'Verify your email'
+        html = `Your OTP is <b> ${token} </b>. It will expire in 1 hour.`
+    }
+    let mailTransporter = nodemailer.createTransport({
+        name: "smtp.ethereal.email",
+        host: "smtp.ethereal.email",
+        port: 587,
+        auth: {
+            user: process.env.ETHEREAL_EMAIL, // generated ethereal user
+            pass: process.env.ETHEREAL_PASSWORD, // generated ethereal password
+        },
+    });
 
-  if (type === "user_otp_verification") {
-    subject = `Verify your email`
-    html = `Your OTP is <b>${token} </b>. It will expire in 1 hour.`
-  }
-  if (type === "user_forgot_password") {
-    subject = 'Verify your email'
-    html = `Your OTP is <b> ${token} </b>. It will expire in 1 hour.`
-  }
+    let mailDetails = {
+        from: `${process.env.SENDER_NAME} <${process.env.ETHEREAL_EMAIL}>`,
+        to: user.email,
+        subject,
+        html
+    };
 
-  const email = {
-    to: userObject.email,
-    from: process.env.EMAIL_FROM,
-    subject,
-    html,
-  }
+    mailTransporter.sendMail(mailDetails, function (err, data) {
+        if (err) {
+            logger.error('error in nodemailer: ', err);
+            throw err;
+        } else {
+            console.log('Email sent successfully');
+        }
+    });
 
-  try {
-    await mailer.send(email);
-    return ('Email sent successfully!!')
-  } catch (error) {
-    logger.error("error in sendgrid mailer: ", error)
-    throw error
-  }
 }
